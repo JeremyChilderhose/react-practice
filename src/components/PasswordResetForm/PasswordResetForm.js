e5211299dc46eb062f13bfd653bdca3f4c433940
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-08-28 23:43:26 -0400

    Add i18n support, reset button & back to login button to PasswordResetForm

--- src/components/PasswordResetForm/PasswordResetForm.js
+++ src/components/PasswordResetForm/PasswordResetForm.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react';
 import styles from './PasswordResetForm.module.css';
 import InputField from '../shared/InputField/InputField';
+import { useTranslation } from 'react-i18next'; 
+import SubmitButton from '../shared/SubmitButton/SubmitButton';
+import { useNavigate } from 'react-router-dom';
+import TextButton from '../shared/TextButton/TextButton';
 
 const PasswordResetForm = () => {
    const [email, setEmail] = useState('')
+   const { t } = useTranslation()
+   const navigate = useNavigate();
+
+   const clickReset = () => {
+      //ToDo: send reset request
+      navigate('/')
+   }
+
+   const clickBackToLogin = () => {
+      navigate('/')
+   }
  
    return (
       <div>
          <div className={styles.resetPasswordBox}>
-            <h2>Reset Password</h2>
-
+            <h2>{t('passwordReset.forgotPassword')}</h2>
             <InputField 
               inputFieldType="email"
               value={email}
               onChangeFunction={setEmail}
-              placeholder="Email on your account"
-              ariaLabel="Email Field"
+              placeholder={t('passwordReset.emailPlaceholder')}
+              ariaLabel={t('passwordReset.emailPlaceholder')}
+            />
+
+            <SubmitButton 
+               isDisabled={email === ''} 
+               buttonText={t('passwordReset.passwordResetButton')}
+               buttonAriaLabel={t('passwordReset.passwordResetButtonArialLabel')}
+               submitFunction={clickReset} 
+            />
+
+            <TextButton 
+               textContent={t('passwordReset.backToLoginButton')}
+               ariaLabel={t('passwordReset.backToLoginButtonAriaLabel')}
+               onClickFunction={clickBackToLogin}
             />
          </div>
       </div>
