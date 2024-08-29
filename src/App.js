cf343f351486f0586b2d1cc9d4c1169260bb03ea
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-08-28 21:25:21 -0400

    Add i18n configuration and locales directory (en & fr)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm'
 import PasswordResetForm from './components/PasswordResetForm/PasswordResetForm'
+import './utils/i18n'
 
 function App() {
   return (
