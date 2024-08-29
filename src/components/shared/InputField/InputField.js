397b2437cab679b00ad4a1dab7e423ba46b365ec
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-08-28 22:06:33 -0400

    Add props for aria labels to inputField component

--- src/components/shared/InputField/InputField.js
+++ src/components/shared/InputField/InputField.js
@@ -14,18 +14,20 @@ const InputField = ({
     onChangeFunction = () => {},
     onClickFunction = () => {},
     placeholder = '',
-    ariaLabel = 'Input Field',
+    fieldAriaLabel = 'Input Field',
+    buttonAriaLabel = 'Input Toggle Button',
+    iconLabel = 'Icon Label'
 }) => {
     return (
         <div>
             { (inputFieldType === "username" || inputFieldType === "email") && (
                 <div className={styles.inputContainer}>
                     <div className={styles.inputWrapper}>
-                    {inputFieldType === "username" && <img src={ProfileIcon} alt="Profile Icon" className={styles.icon} /> }
-                    {inputFieldType === "email" && <img src={EmailIcon} alt="Email Icon" className={styles.icon} /> }
+                    {inputFieldType === "username" && <img src={ProfileIcon} alt={iconLabel} className={styles.icon} /> }
+                    {inputFieldType === "email" && <img src={EmailIcon} alt={iconLabel} className={styles.icon} /> }
                     <input 
-                        aria-label={ariaLabel}
-                        name={ariaLabel}
+                        aria-label={fieldAriaLabel}
+                        name={fieldAriaLabel}
                         type="text"
                         value={value}
                         onChange={(e) => onChangeFunction(e.target.value)}
@@ -38,25 +40,25 @@ const InputField = ({
 
             { inputFieldType === "password" && (
             <div className={styles.inputWrapper}>
-                <img src={LockIcon} alt="Lock Icon" className={styles.icon} />
+                <img src={LockIcon} alt={iconLabel} className={styles.icon} />
                 <input 
                     type={isPasswordHidden ? "password" : "text"}
-                    name={ariaLabel}
+                    name={fieldAriaLabel}
                     value={value}
                     onChange={(e) => onChangeFunction(e.target.value)}  
                     required
                     placeholder={placeholder}  
-                    aria-label={ariaLabel}  
+                    aria-label={fieldAriaLabel}  
                 />
                 <button
                     type="button"
                     className={styles.eyeButton}
                     onClick={onClickFunction}  
-                    aria-label={isPasswordHidden ? "Show Password Button" : "Hide Password Button"}
+                    aria-label={buttonAriaLabel}
                 >
                     {isPasswordHidden 
-                        ? <img src={OpenEye} alt="Show Password button" className={styles.eyeIcon} />
-                        : <img src={ClosedEye} alt="Hide Password button" className={styles.eyeIcon} />
+                        ? <img src={OpenEye} alt={buttonAriaLabel} className={styles.eyeIcon} />
+                        : <img src={ClosedEye} alt={buttonAriaLabel} className={styles.eyeIcon} />
                     }
                 </button>
             </div>
@@ -66,13 +68,14 @@ const InputField = ({
 }
 
 InputField.propTypes = {
-    inputFieldType: PropTypes.oneOf(["username", "password", "email"]), // Ensure only these values are accepted
-    isPasswordHidden: PropTypes.bool,    // Boolean to toggle password visibility
-    value: PropTypes.string,             // Current value of the input field
-    onChangeFunction: PropTypes.func.isRequired, // Function to handle input changes
-    onClickFunction: PropTypes.func,   // Function to handle button click (optional)
-    placeholder: PropTypes.string,       // Placeholder text for the input field
-    ariaLabel: PropTypes.string,         // Aria label for accessibility
+    inputFieldType: PropTypes.oneOf(["username", "password", "email"]),
+    isPasswordHidden: PropTypes.bool,    
+    value: PropTypes.string,             
+    onChangeFunction: PropTypes.func.isRequired, 
+    onClickFunction: PropTypes.func,   
+    placeholder: PropTypes.string,       
+    fieldAriaLabel: PropTypes.string,  
+    iconLabel: PropTypes.string       
 };
  
 export default InputField;
