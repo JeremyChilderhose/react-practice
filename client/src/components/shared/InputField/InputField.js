e418250c6921dd4b5ad91cb5091ef6b8f2ad67a1
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-08-31 22:52:09 -0400

    Update inputField component to red underline errors

--- client/src/components/shared/InputField/InputField.js
+++ client/src/components/shared/InputField/InputField.js
@@ -5,12 +5,14 @@ import ProfileIcon from '../../../assets/icons/ProfileIcon.svg';
 import LockIcon from '../../../assets/icons/LockIcon.svg'; 
 import OpenEye from '../../../assets/icons/OpenEye.svg';
 import ClosedEye from '../../../assets/icons/ClosedEye.svg';
-import EmailIcon from '../../../assets/icons/EmailIcon.svg'
+import EmailIcon from '../../../assets/icons/EmailIcon.svg';
 
 const InputField = ({
     inputFieldType = "Input",
     isPasswordHidden = true,
     value = '',
+    hasError = false,
+    validateFunction = () => {},
     onChangeFunction = () => {},
     onClickFunction = () => {},
     placeholder = '',
@@ -18,52 +20,58 @@ const InputField = ({
     buttonAriaLabel = 'Input Toggle Button',
     iconLabel = 'Icon Label'
 }) => {
+    const handleBlur = () => {
+        validateFunction(); // Call validateFunction on blur
+    };
+
     return (
-        <div>
+        <div className={styles.inputContainer}>
             { (inputFieldType === "username" || inputFieldType === "email") && (
-                <div className={styles.inputContainer}>
+                <div className={`${styles.inputWrapper} ${hasError ? styles.error : ''}`}>
                     <div className={styles.inputWrapper}>
-                    {inputFieldType === "username" && <img src={ProfileIcon} alt={iconLabel} className={styles.icon} /> }
-                    {inputFieldType === "email" && <img src={EmailIcon} alt={iconLabel} className={styles.icon} /> }
-                    <input 
-                        aria-label={fieldAriaLabel}
-                        name={fieldAriaLabel}
-                        type={inputFieldType === 'email' ? 'email' : 'text'}
-                        value={value}
-                        onChange={(e) => onChangeFunction(e.target.value)}
-                        required
-                        placeholder={placeholder}
-                    />
+                        {inputFieldType === "username" && <img src={ProfileIcon} alt={iconLabel} className={styles.icon} />}
+                        {inputFieldType === "email" && <img src={EmailIcon} alt={iconLabel} className={styles.icon} />}
+                        <input 
+                            aria-label={fieldAriaLabel}
+                            name={fieldAriaLabel}
+                            type={inputFieldType === 'email' ? 'email' : 'text'}
+                            value={value}
+                            onBlur={handleBlur} // Use handleBlur here
+                            onChange={(e) => onChangeFunction(e.target.value)}
+                            required
+                            placeholder={placeholder}
+                        />
                     </div>
                 </div>
             )}
 
             { inputFieldType === "password" && (
-            <div className={styles.inputWrapper}>
-                <img src={LockIcon} alt={iconLabel} className={styles.icon} />
-                <input 
-                    type={isPasswordHidden ? "password" : "text"}
-                    name={fieldAriaLabel}
-                    value={value}
-                    onChange={(e) => onChangeFunction(e.target.value)}  
-                    required
-                    placeholder={placeholder}  
-                    aria-label={fieldAriaLabel}  
-                />
-                <button
-                    type="button"
-                    className={styles.eyeButton}
-                    onClick={onClickFunction}  
-                    aria-label={buttonAriaLabel}
-                >
-                    {isPasswordHidden 
-                        ? <img src={OpenEye} alt={buttonAriaLabel} className={styles.eyeIcon} />
-                        : <img src={ClosedEye} alt={buttonAriaLabel} className={styles.eyeIcon} />
-                    }
-                </button>
-            </div>
-        )} 
-    </div> 
+                <div className={`${styles.inputWrapper} ${hasError ? styles.error : ''}`}>
+                    <img src={LockIcon} alt={iconLabel} className={styles.icon} />
+                    <input 
+                        type={isPasswordHidden ? "password" : "text"}
+                        name={fieldAriaLabel}
+                        value={value}
+                        onBlur={handleBlur} // Use handleBlur here
+                        onChange={(e) => onChangeFunction(e.target.value)}  
+                        required
+                        placeholder={placeholder}  
+                        aria-label={fieldAriaLabel}  
+                    />
+                    <button
+                        type="button"
+                        className={styles.eyeButton}
+                        onClick={onClickFunction}  
+                        aria-label={buttonAriaLabel}
+                    >
+                        {isPasswordHidden 
+                            ? <img src={OpenEye} alt={buttonAriaLabel} className={styles.eyeIcon} />
+                            : <img src={ClosedEye} alt={buttonAriaLabel} className={styles.eyeIcon} />
+                        }
+                    </button>
+                </div>
+            )} 
+        </div> 
     );
 }
 
@@ -71,6 +79,8 @@ InputField.propTypes = {
     inputFieldType: PropTypes.oneOf(["username", "password", "email"]),
     isPasswordHidden: PropTypes.bool,    
     value: PropTypes.string,             
+    hasError: PropTypes.bool, 
+    validateFunction: PropTypes.func,  // Updated PropTypes
     onChangeFunction: PropTypes.func.isRequired, 
     onClickFunction: PropTypes.func,   
     placeholder: PropTypes.string,       
