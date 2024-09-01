ad4a129a22192371997d67a0951dedf19cce9c6b
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-08-31 20:31:42 -0400

    Add CORSMiddleware to main file to allow http requests from react app

--- server/main.py
+++ server/main.py
@@ -2,8 +2,22 @@ import os
 from fastapi import FastAPI
 from db.initialize_db import setup_db
 from server.routes import users, tweets
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",  
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Specify allowed origins
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 engine = setup_db('dev')
 
