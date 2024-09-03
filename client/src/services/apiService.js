092025e93602077ae9d7edd5dfcaa1841f9e7ae0
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-09-02 20:09:54 -0400

    Add login to apiService

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -36,6 +36,63 @@ class ApiService {
             throw error;
         }
     }
+
+    async loginUser(username, password) {
+        const url = `${this.baseUrl}/login`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    hashed_password: password,
+                }),
+            });
+            
+            if (!response.ok) {
+                const errorText = await response.text(); 
+                throw new Error(`HTTP error! Status: ${response.status}, Details: ${errorText}`);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error registering user:', error);
+            throw error;
+        }
+    }
+
+    async verifyUserToken(token) {
+        const url = `${this.baseUrl}/verify-token`;
+
+        if (!token) {
+            throw new Error('No token found in local storage');
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(`HTTP error! Status: ${response.status}, Details: ${errorText}`);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error verifying token:', error);
+            throw error;
+        }
+    }
 }
 
 const apiService = new ApiService(apiBaseUrl);
