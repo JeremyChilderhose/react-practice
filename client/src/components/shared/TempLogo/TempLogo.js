610b237c3e4b10dcae4fd8837b3e7170e16c7188
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-09-03 08:27:36 -0400

    Update TempLogo to have gradient version

--- client/src/components/shared/TempLogo/TempLogo.js
+++ client/src/components/shared/TempLogo/TempLogo.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import styles from './TempLogo.module.css'; 
+import PropTypes from 'prop-types';
+import styles from './TempLogo.module.css';
 
-const TempLogo = () => (
+const TempLogo = ({ isNavbar }) => {
+  return (
     <div className={styles.logoDiv}>
-        <div className={styles.logo}>
-            <h1>TC</h1>
-        </div>
+      <div className={isNavbar ? styles.navbarLogo : styles.logo}>
+        <h1>TC</h1>
+      </div>
     </div>
-);
+  );
+};
+
+TempLogo.propTypes = {
+  isNavbar: PropTypes.bool,
+};
 
 export default TempLogo;
