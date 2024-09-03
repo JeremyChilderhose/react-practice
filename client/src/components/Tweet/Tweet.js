133e006c211b6f8a62d44422eac9201ed979f798
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-09-03 08:28:56 -0400

    Add temporary tweet content and style component

--- client/src/components/Tweet/Tweet.js
+++ client/src/components/Tweet/Tweet.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import styles from './Tweet.module.css';
+import userIcon from '../../assets/images/userIcon.png';
+import emptyHeartIcon from '../../assets/icons/EmptyHeartIcon.svg';
 
 const Tweet = () => (
- <div data-testid="Tweet">
-    Tweet Component
- </div>
+  <div className={styles.container}>
+    <div className={styles.profileIcon}>
+      <img src={userIcon} alt="Profile" />
+    </div>
+    <div className={styles.formContent}>
+      <div className={styles.username}>Username</div>
+      <div className={styles.tweetText}>
+         Excited to announce our new product launch today! It's been a long journey, but we're finally here. Thanks to everyone who made this possible! 🎉🚀 #NewBeginnings
+      </div>
+      <div className={styles.likeButtonContainer}>
+        <img src={emptyHeartIcon} alt="Like" className={styles.likeButton} />
+        <span className={styles.likeCounter}>0</span>
+      </div>
+    </div>
+  </div>
 );
-
-Tweet.propTypes = {};
 
 export default Tweet;
