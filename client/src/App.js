f763b13c5ccda164830d1a5ab68b781286e7ab35
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-09-03 08:32:27 -0400

    Update routes and add navbar to App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm'
 import PasswordResetForm from './components/PasswordResetForm/PasswordResetForm'
-import UserProfile from './components/UserProfile/UserProfile'
+import Navbar from './components/Navbar/Navbar'
 import './utils/i18n'
-// import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
@@ -12,13 +13,14 @@ function App() {
       <header className="App-header">
       </header>
       <Router>
-      {/* <Navbar /> */}
+      <Navbar />
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/:variant" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/:variant" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/resetPassword" element={<PasswordResetForm />} />
-          <Route path="/profile" element={<UserProfile />} />
         </Routes>
       </Router>
     </div>
