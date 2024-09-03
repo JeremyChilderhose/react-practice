49e0ad35ab841fa4088af7f0522a6a4658259b6f
Jeremy Childerhose <jeremychilderhose@gmail.com>
2024-09-03 08:29:14 -0400

    Add tweet form component

--- client/src/components/TweetForm/TweetForm.js
+++ client/src/components/TweetForm/TweetForm.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styles from './TweetForm.module.css';
+import userIcon from '../../assets/images/userIcon.png';
+import { useTranslation } from 'react-i18next';
 
-const TweetForm = () => (
- <div data-testid="TweetForm">
-    TweetForm Component
- </div>
-);
+const TweetForm = () => {
+  const { t } = useTranslation();
 
-TweetForm.propTypes = {};
+  return (
+    <div className={styles.container}>
+      <div className={styles.profileIcon}>
+        <img src={userIcon} alt="Profile" />
+      </div>
+      <div className={styles.formContent}>
+        <div className={styles.username}>Username</div>
+        <textarea name={t('tweetForm.textAreaName')}
+          className={styles.inputField}
+          placeholder={t('tweetForm.contentPlaceholder')}
+        />
+      </div>
+      <button className={styles.sendButton}>{t('tweetForm.send')}</button>
+    </div>
+  );
+};
 
 export default TweetForm;
